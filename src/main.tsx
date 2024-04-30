6b9588410194de20b491eb11366f301a185d61f6
Brilliant Hanif Almubarak <brillianthanif@gmail.com>
2024-04-30 17:36:03 +0700

    feat : add page in contact

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app.tsx";
 import "./index.css";
+import { Root } from "./routes/root";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./pages/error";
+import Page from "./routes/main";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "contacts/:contactId",
+    element: <Page />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    
+
   </React.StrictMode>
 );
---
+++ src/pages/error.tsx
@@ -0,0 +1,18 @@
+import { useLocation } from "react-router-dom";
+
+export default function ErrorPage(){
+    const location = useLocation();
+    const error = location.state && location.state.error;
+
+    console.error(error);
+  
+    return (
+      <div id="error-page">
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>{error?.statusText || error?.message}</i>
+        </p>
+      </div>
+    );
+}
